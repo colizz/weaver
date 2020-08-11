34449396d2bd8955ef37a1b7c5758794a6acd60b
Congqiao Li <congqiao.li@cern.ch>
2020-08-12 02:50:03 +0800

    simplify default architecture

--- networks/particle_net_vbspolar_pf_sv.py
+++ networks/particle_net_vbspolar_pf_sv.py
@@ -10,9 +10,9 @@ def weight_init(m):
 
 def get_model(data_config, **kwargs):
     conv_params = [
-        (16, (64, 64, 64)),
-        (16, (128, 128, 128)),
-        (16, (256, 256, 256)),
+        (8, (64, 64, 64)),
+        (8, (96, 96, 96)),
+        (8, (128, 128, 128)),
         ]
     fc_params = [(256, 0.1)]
     use_fusion = True
