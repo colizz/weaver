05fedbe0258489bed61e025636d847b3717f83d1
Congqiao Li <congqiao.li@cern.ch>
2020-08-26 16:19:37 +0800

    enable var length is None

--- utils/data/config.py
+++ utils/data/config.py
@@ -66,6 +66,8 @@ class DataConfig(object):
         self.input_dicts = {k: [] for k in self.input_names}
         self.input_shapes = {}
         for k, o in opts['inputs'].items():
+            if o['length'] == 'None':
+                o['length'] = None
             self.input_shapes[k] = (-1, len(o['vars']), o['length'])
             for v in o['vars']:
                 v = _as_list(v)
