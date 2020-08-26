1a9d79e27870edc226a75f5a1e7df2fccbdc8561
Congqiao Li <congqiao.li@cern.ch>
2020-08-26 16:17:16 +0800

    add ParticleNet feature: possible to return before fusion layer

--- utils/nn/model/ParticleNet.py
+++ utils/nn/model/ParticleNet.py
@@ -132,6 +132,7 @@ class ParticleNet(nn.Module):
                  use_counts=True,
                  for_inference=False,
                  for_segmentation=False,
+                 return_before_fusion=False,
                  **kwargs):
         super(ParticleNet, self).__init__(**kwargs)
 
@@ -148,30 +149,32 @@ class ParticleNet(nn.Module):
             self.edge_convs.append(EdgeConvBlock(k=k, in_feat=in_feat, out_feats=channels, cpu_mode=for_inference))
 
         self.use_fusion = use_fusion
-        if self.use_fusion:
+        self.return_before_fusion = return_before_fusion
+        if self.use_fusion and not self.return_before_fusion:
             in_chn = sum(x[-1] for _, x in conv_params)
             out_chn = np.clip((in_chn // 128) * 128, 128, 1024)
             self.fusion_block = nn.Sequential(nn.Conv1d(in_chn, out_chn, kernel_size=1, bias=False), nn.BatchNorm1d(out_chn), nn.ReLU())
 
         self.for_segmentation = for_segmentation
 
-        fcs = []
-        for idx, layer_param in enumerate(fc_params):
-            channels, drop_rate = layer_param
-            if idx == 0:
-                in_chn = out_chn if self.use_fusion else conv_params[-1][1][-1]
-            else:
-                in_chn = fc_params[idx - 1][0]
+        if not self.return_before_fusion:
+            fcs = []
+            for idx, layer_param in enumerate(fc_params):
+                channels, drop_rate = layer_param
+                if idx == 0:
+                    in_chn = out_chn if self.use_fusion else conv_params[-1][1][-1]
+                else:
+                    in_chn = fc_params[idx - 1][0]
+                if self.for_segmentation:
+                    fcs.append(nn.Sequential(nn.Conv1d(in_chn, channels, kernel_size=1, bias=False),
+                                             nn.BatchNorm1d(channels), nn.ReLU(), nn.Dropout(drop_rate)))
+                else:
+                    fcs.append(nn.Sequential(nn.Linear(in_chn, channels), nn.ReLU(), nn.Dropout(drop_rate)))
             if self.for_segmentation:
-                fcs.append(nn.Sequential(nn.Conv1d(in_chn, channels, kernel_size=1, bias=False),
-                                         nn.BatchNorm1d(channels), nn.ReLU(), nn.Dropout(drop_rate)))
+                fcs.append(nn.Conv1d(fc_params[-1][0], num_classes, kernel_size=1))
             else:
-                fcs.append(nn.Sequential(nn.Linear(in_chn, channels), nn.ReLU(), nn.Dropout(drop_rate)))
-        if self.for_segmentation:
-            fcs.append(nn.Conv1d(fc_params[-1][0], num_classes, kernel_size=1))
-        else:
-            fcs.append(nn.Linear(fc_params[-1][0], num_classes))
-        self.fc = nn.Sequential(*fcs)
+                fcs.append(nn.Linear(fc_params[-1][0], num_classes))
+            self.fc = nn.Sequential(*fcs)
 
         self.for_inference = for_inference
 
@@ -197,6 +200,10 @@ class ParticleNet(nn.Module):
             fts = conv(pts, fts) * mask
             if self.use_fusion:
                 outputs.append(fts)
+        if self.return_before_fusion:
+            assert self.use_fusion == True
+            return outputs
+
         if self.use_fusion:
             fts = self.fusion_block(torch.cat(outputs, dim=1)) * mask
 
@@ -326,3 +333,170 @@ class ParticleNetTaggerVBSPolar(nn.Module):
         features = torch.cat((self.pf_conv(pf1_features * pf1_mask) * pf1_mask, self.pf_conv(pf2_features * pf2_mask) * pf2_mask, self.sv_conv(sv1_features * sv1_mask) * sv1_mask, self.sv_conv(sv2_features * sv2_mask) * sv2_mask), dim=2)
         mask = torch.cat((pf1_mask, pf2_mask, sv1_mask, sv2_mask), dim=2)
         return self.pn(points, features, mask)
+
+class ParticleNetTaggerVBSPolar_addJetLep(nn.Module):
+
+    def __init__(self,
+                 pf_features_dims,
+                 sv_features_dims,
+                 jet_features_dims,
+                 lep_features_dims,
+                 num_classes,
+                 conv_params=[(7, (32, 32, 32)), (7, (64, 64, 64))],
+                 fc_params=[(128, 0.1)],
+                 use_fusion=True,
+                 use_fts_bn=True,
+                 use_counts=True,
+                 pf_input_dropout=None,
+                 sv_input_dropout=None,
+                 for_inference=False,
+                 **kwargs):
+        super(ParticleNetTaggerVBSPolar_addJetLep, self).__init__(**kwargs)
+        self.pf_input_dropout = nn.Dropout(pf_input_dropout) if pf_input_dropout else None
+        self.sv_input_dropout = nn.Dropout(sv_input_dropout) if sv_input_dropout else None
+        self.pf_conv = FeatureConv(pf_features_dims, 32)
+        self.sv_conv = FeatureConv(sv_features_dims, 32)
+        self.jet_conv = FeatureConv(jet_features_dims, 32)
+        self.lep_conv = FeatureConv(lep_features_dims, 32)
+        self.pn = ParticleNet(input_dims=32,
+                              num_classes=num_classes,
+                              conv_params=conv_params,
+                              fc_params=fc_params,
+                              use_fusion=use_fusion,
+                              use_fts_bn=use_fts_bn,
+                              use_counts=use_counts,
+                              for_inference=for_inference)
+
+    def forward(self, pf1_points, pf1_features, pf1_mask, pf2_points, pf2_features, pf2_mask, sv1_points, sv1_features, sv1_mask, sv2_points, sv2_features, sv2_mask, jet_points, jet_features, jet_mask, lep_points, lep_features, lep_mask):
+        if self.pf_input_dropout:
+            pf1_mask = (self.pf_input_dropout(pf1_mask) != 0).float()
+            pf1_points *= pf1_mask
+            pf1_features *= pf1_mask
+            pf2_mask = (self.pf_input_dropout(pf2_mask) != 0).float()
+            pf2_points *= pf2_mask
+            pf2_features *= pf2_mask
+        if self.sv_input_dropout:
+            sv1_mask = (self.sv_input_dropout(sv1_mask) != 0).float()
+            sv1_points *= sv1_mask
+            sv1_features *= sv1_mask
+            sv2_mask = (self.sv_input_dropout(sv2_mask) != 0).float()
+            sv2_points *= sv2_mask
+            sv2_features *= sv2_mask
+
+        points = torch.cat((pf1_points, pf2_points, sv1_points, sv2_points, jet_points, lep_points), dim=2)
+        features = torch.cat((self.pf_conv(pf1_features * pf1_mask) * pf1_mask, self.pf_conv(pf2_features * pf2_mask) * pf2_mask, self.sv_conv(sv1_features * sv1_mask) * sv1_mask, self.sv_conv(sv2_features * sv2_mask) * sv2_mask, self.jet_conv(jet_features), self.lep_conv(lep_features)), dim=2)
+        mask = torch.cat((pf1_mask, pf2_mask, sv1_mask, sv2_mask, jet_mask, lep_mask), dim=2)
+        return self.pn(points, features, mask)
+
+class ParticleNetTaggerVBSPolarV2(nn.Module):
+
+    def __init__(self,
+                 pf_features_dims,
+                 sv_features_dims,
+                 num_classes,
+                 conv_params=[(7, (32, 32, 32)), (7, (64, 64, 64))],
+                 fc_params=[(128, 0.1)],
+                 use_fusion=True,
+                 use_fts_bn=True,
+                 use_counts=True,
+                 pf_input_dropout=None,
+                 sv_input_dropout=None,
+                 for_inference=False,
+                 for_segmentation=False,
+                 **kwargs):
+        super(ParticleNetTaggerVBSPolarV2, self).__init__(**kwargs)
+        self.pf_input_dropout = nn.Dropout(pf_input_dropout) if pf_input_dropout else None
+        self.sv_input_dropout = nn.Dropout(sv_input_dropout) if sv_input_dropout else None
+        self.pf_conv1 = FeatureConv(pf_features_dims, 32)
+        self.pf_conv2 = FeatureConv(pf_features_dims, 32)
+        self.sv_conv1 = FeatureConv(sv_features_dims, 32)
+        self.sv_conv2 = FeatureConv(sv_features_dims, 32)
+        self.pn1 = ParticleNet(input_dims=32,
+                               num_classes=num_classes,
+                               conv_params=conv_params,
+                               fc_params=fc_params,
+                               use_fusion=use_fusion,
+                               use_fts_bn=use_fts_bn,
+                               use_counts=use_counts,
+                               for_inference=for_inference,
+                               return_before_fusion=True)
+        self.pn2 = ParticleNet(input_dims=32,
+                               num_classes=num_classes,
+                               conv_params=conv_params,
+                               fc_params=fc_params,
+                               use_fusion=use_fusion,
+                               use_fts_bn=use_fts_bn,
+                               use_counts=use_counts,
+                               for_inference=for_inference,
+                               return_before_fusion=True)
+
+        self.use_fusion = use_fusion
+        assert self.use_fusion == True
+        if self.use_fusion:
+            in_chn = 2 * sum(x[-1] for _, x in conv_params)  # fusing two sets of output
+            out_chn = np.clip((in_chn // 128) * 128, 128, 1024)
+            self.fusion_block = nn.Sequential(nn.Conv1d(in_chn, out_chn, kernel_size=1, bias=False), nn.BatchNorm1d(out_chn), nn.ReLU())
+
+        self.for_segmentation = for_segmentation
+        fcs = []
+        for idx, layer_param in enumerate(fc_params):
+            channels, drop_rate = layer_param
+            if idx == 0:
+                in_chn = out_chn if self.use_fusion else conv_params[-1][1][-1]
+            else:
+                in_chn = fc_params[idx - 1][0]
+            if self.for_segmentation:
+                fcs.append(nn.Sequential(nn.Conv1d(in_chn, channels, kernel_size=1, bias=False),
+                                         nn.BatchNorm1d(channels), nn.ReLU(), nn.Dropout(drop_rate)))
+            else:
+                fcs.append(nn.Sequential(nn.Linear(in_chn, channels), nn.ReLU(), nn.Dropout(drop_rate)))
+        if self.for_segmentation:
+            fcs.append(nn.Conv1d(fc_params[-1][0], num_classes, kernel_size=1))
+        else:
+            fcs.append(nn.Linear(fc_params[-1][0], num_classes))
+        self.fc = nn.Sequential(*fcs)
+
+        self.for_inference = for_inference
+        self.use_counts = use_counts
+
+    def forward(self, pf1_points, pf1_features, pf1_mask, pf2_points, pf2_features, pf2_mask, sv1_points, sv1_features, sv1_mask, sv2_points, sv2_features, sv2_mask):
+        if self.pf_input_dropout:
+            pf1_mask = (self.pf_input_dropout(pf1_mask) != 0).float()
+            pf1_points *= pf1_mask
+            pf1_features *= pf1_mask
+            pf2_mask = (self.pf_input_dropout(pf2_mask) != 0).float()
+            pf2_points *= pf2_mask
+            pf2_features *= pf2_mask
+        if self.sv_input_dropout:
+            sv1_mask = (self.sv_input_dropout(sv1_mask) != 0).float()
+            sv1_points *= sv1_mask
+            sv1_features *= sv1_mask
+            sv2_mask = (self.sv_input_dropout(sv2_mask) != 0).float()
+            sv2_points *= sv2_mask
+            sv2_features *= sv2_mask
+
+        # print(pf1_points.shape, pf1_features.shape, pf1_mask.shape) # mask shape: (N, 1, P)
+        points1 = torch.cat((pf1_points, sv1_points), dim=2)
+        features1 = torch.cat((self.pf_conv1(pf1_features * pf1_mask) * pf1_mask, self.sv_conv1(sv1_features * sv1_mask) * sv1_mask), dim=2)
+        mask1 = torch.cat((pf1_mask, sv1_mask), dim=2)
+        points2 = torch.cat((pf2_points, sv2_points), dim=2)
+        features2 = torch.cat((self.pf_conv2(pf2_features * pf2_mask) * pf2_mask, self.sv_conv2(sv2_features * sv2_mask) * sv2_mask), dim=2)
+        mask2 = torch.cat((pf2_mask, sv2_mask), dim=2)
+
+        outputs_pn1 = self.pn1(points1, features1, mask1)
+        outputs_pn2 = self.pn2(points2, features2, mask2)
+
+        # print(outputs_pn1[0].shape, outputs_pn1[1].shape, outputs_pn1[2].shape) # (N, 96, P)
+        if self.use_fusion:
+            fts = self.fusion_block(torch.cat(outputs_pn1 + outputs_pn2, dim=1))
+
+        if self.for_segmentation:
+            x = fts
+        else:
+            x = fts.mean(dim=-1)
+
+        output = self.fc(x)
+        if self.for_inference:
+            output = torch.softmax(output, dim=1)
+        # print('output:\n', output)
+        return output
