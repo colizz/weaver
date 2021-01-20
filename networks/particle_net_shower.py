1680e08f41a615e243eed1f5d6f1a015fba370ae
Congqiao Li <congqiao.li@cern.ch>
2021-01-21 02:24:20 +0800

    Changes from lambda to hh4b (use 1D ParticleNet)

--- networks/particle_net_shower.py
+++ networks/particle_net_shower.py
@@ -4,9 +4,9 @@ from utils.nn.model.ParticleNet import ParticleNetTagger_1feat
 
 def get_model(data_config, **kwargs):
     conv_params = [
-        (8, (64, 64, 64)),
-        (8, (96, 96, 96)),
-        (8, (128, 128, 128)),
+        (16, (64, 64, 64)),
+        (16, (128, 128, 128)),
+        (16, (256, 256, 256)),
         ]
     fc_params = [(256, 0.1)]
     use_fusion = True
